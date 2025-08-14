e20eef567ff646e1448a274b80719ba4600d8aa7
Thanzia <thanzia123@gmail.com>
2025-08-14 21:28:54 +0400

    eigth commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -153,7 +153,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic will put files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
