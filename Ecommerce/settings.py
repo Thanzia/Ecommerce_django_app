76757bce4704a9546cba9b3277023de4c9d35524
Thanzia <thanzia123@gmail.com>
2025-08-18 16:23:34 +0400

    updation4

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -161,6 +161,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic will put files
 
+# Where you put your own static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
